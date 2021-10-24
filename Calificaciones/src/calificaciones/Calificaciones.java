34a62df932e4e2f97973398d1e4da8174e3d705e
gAl0402 <93062105+gAl0402@users.noreply.github.com>
2021-10-23 20:31:40 -0500

    Add files via upload
    CARPETA CALIFICACIONES

---
+++ Calificaciones/src/calificaciones/Calificaciones.java
@@ -0,0 +1,15 @@
+
+package calificaciones;
+import java.util.*;
+/**
+ *
+ * @author galic
+ */
+public class Calificaciones {
+        String nombre = "";
+       
+        
+        
+        
+    
+}
